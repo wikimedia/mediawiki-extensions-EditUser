a78aa59f56d225463cc7ebd83393096acb952bcc
Paladox <thomasmulhall410@yahoo.com>
2015-11-29 21:08:32 +0000

    Add extension.json, Remove php entry point
    Change-Id: I418d86cb1d5b00d2c4506f0e0a1aad66f9e5d71c

--- EditUser.php
+++ EditUser.php
@@ -1,35 +1,18 @@
 <?php
 /**
-* EditUser extension by Ryan Schmidt
-*/
+ * EditUser extension by Ryan Schmidt
+ */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo "This file is an extension to the MediaWiki software and is not a valid access point";
-	die( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'EditUser' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['EditUser'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['EditUserAliases'] = __DIR__ . '/EditUser.alias.php';
+	/* wfWarn(
+		'Deprecated PHP entry point used for EditUser extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the EditUser extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['specialpage'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'EditUser',
-	'namemsg'        => 'edituser-extensionname',
-	'version'        => '1.8.0',
-	'author'         => 'Ryan Schmidt',
-	'descriptionmsg' => 'edituser-desc',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:EditUser',
-);
-
-// Internationlization files
-$wgMessagesDirs['EditUser'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['EditUserAliases'] = __DIR__ . '/EditUser.alias.php';
-
-// Special page classes
-$wgAutoloadClasses['EditUser'] = __DIR__ . '/EditUser_body.php';
-$wgAutoloadClasses['EditUserPreferencesForm'] = __DIR__ . '/EditUserPreferencesForm.php';
-
-$wgSpecialPages['EditUser'] = 'EditUser';
-
-// Default group permissions
-$wgAvailableRights[] = 'edituser';
-$wgAvailableRights[] = 'edituser-exempt';
-$wgGroupPermissions['bureaucrat']['edituser'] = true;
-$wgGroupPermissions['sysop']['edituser-exempt'] = true;
